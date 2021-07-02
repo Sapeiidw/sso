a88797a717e55741202fbbbc6ec230f57ea362d7
Sapeiidw <sapeiidw@gmail.com>
2021-07-02 14:59:04 +0800

    nambah home page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('/home', 'home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
